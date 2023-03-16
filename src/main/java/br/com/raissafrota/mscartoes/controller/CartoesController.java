1d5d48bb4133ad701a6817f93342a128db5593ab
Raissa <raissa.frota@capgemini.com>
2023-03-16 14:22:48 -0300

    Implementação do get para Cartões com renda até algum valor

--- src/main/java/br/com/raissafrota/mscartoes/controller/CartoesController.java
+++ src/main/java/br/com/raissafrota/mscartoes/controller/CartoesController.java
@@ -3,6 +3,7 @@ package br.com.raissafrota.mscartoes.controller;
 import br.com.raissafrota.mscartoes.dto.request.CartaoRequest;
 import br.com.raissafrota.mscartoes.entity.Cartao;
 import br.com.raissafrota.mscartoes.service.CartaoService;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -10,6 +11,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -29,5 +31,11 @@ public class CartoesController {
         Cartao cartao = request.toModel();
         cartaoService.save(cartao);
         return ResponseEntity.status(HttpStatus.CREATED).build();
+    }
+
+    @GetMapping(params = "renda")
+    public ResponseEntity<List<Cartao>> getCartoesRendaAteh(@RequestParam("renda") Long renda){
+        List<Cartao> list = cartaoService.getCartoesRendaMenorIgual(renda);
+        return ResponseEntity.ok(list);
     }
 }
